293a0bf8173e7932bf18a7da23abd36b3ec192cc
Cameron Reikes <cameronreikes@gmail.com>
2022-09-25 16:03:45 -0700

    Successful moving and level

--- Assets/GameSession.cs
+++ Assets/GameSession.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
+public class GameInput
+{
+    public Vector2 direction;
+    public bool use;
+}
 
 public class GameSession : MonoBehaviour
 {
-    enum State
+    public enum State
     {
         WaitingForInput,
         Processing,
@@ -14,24 +21,54 @@ public class GameSession : MonoBehaviour
 
     public AnimationCurve deltaCurve;
     private float processProgress = 0.0f;
-    private State state = State.WaitingForInput;
+    public float processSpeed { get; private set; } = 1.5f;
+    public State state { get; private set; } = State.WaitingForInput;
 
-    void Update()
+
+    public delegate void OnNewInput(GameInput input);
+    public event OnNewInput OnInput;
+
+    public void SupplyInput(GameInput input)
+    {
+        if (OnInput != null)
+        {
+            OnInput(input);
+        }
+        processProgress = 0.0f;
+        state = State.Processing;
+    }
+
+    private void PauseTime()
+    {
+        Time.timeScale = 0.0f;
+        Time.fixedDeltaTime = 0.0f;
+    }
+
+    private void Start()
+    {
+        PauseTime();
+    }
+
+    private void Update()
     {
         switch (state)
         {
             case State.WaitingForInput:
+                PauseTime();
                 break;
             case State.Processing:
-                processProgress += Time.unscaledDeltaTime;
-                if(processProgress > 1.0f)
+                processProgress += Time.unscaledDeltaTime * processSpeed;
+                if (processProgress > 1.0f)
                 {
-                    processProgress = 0.0f;
                     state = State.WaitingForInput;
                 }
-                Time.timeScale = deltaCurve.Evaluate(processProgress);
+                float newScale = deltaCurve.Evaluate(processProgress);
+                newScale = Mathf.Max(newScale, 0.01f);
+                Time.timeScale = newScale;
+                Time.fixedDeltaTime = Time.timeScale * 0.02f;
 
                 break;
         }
+
     }
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,90 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // do not change gs after enabled, disable it first to remove input callback
+    public GameSession gs;
+
+    public Rigidbody2D body;
+    public LineRenderer line;
+    public float speed = 10.0f;
+    float lineLength;
+
+    void OnInput(Input input)
     {
-        
+        Debug.Log("Input: " + input);
     }
 
-    // Update is called once per frame
+
+    void Start()
+    {
+        lineLength = Vector3.Magnitude(line.GetPosition(line.positionCount - 1));
+    }
+
     void Update()
     {
-        
+        float target = gs.state switch
+        {
+            GameSession.State.WaitingForInput => 1.0f,
+            GameSession.State.Processing => 0.0f,
+            _ => float.NaN,
+        };
+        Assert.IsTrue(target != float.NaN);
+
+        Color newColor = line.startColor;
+        newColor.a = Mathf.Lerp(newColor.a, target, Time.unscaledDeltaTime * 14.0f);
+        line.startColor = newColor;
+        line.endColor = newColor;
+
+        //line.colorGradient.alphaKeys[0].alpha = Mathf.Lerp(line.colorGradient.alphaKeys[0].alpha, target, Time.unscaledDeltaTime * 100.0f);
+        //line.colorGradient.alphaKeys[1].alpha = line.colorGradient.alphaKeys[0].alpha;
+        //line.colorGradient.SetKeys(line.colorGradient.colorKeys, line.colorGradient.alphaKeys);
+        //Debug.Log(Mathf.Lerp(line.colorGradient.alphaKeys[0].alpha, target, Time.unscaledDeltaTime * 100.0f));
+
+        switch (gs.state)
+        {
+            case GameSession.State.WaitingForInput:
+                Vector3 dir = new Vector3(); // in world space
+                // calculate dir, to mouse
+                {
+                    Vector3 world3DMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    world3DMouse.z = 0.0f;
+                    dir = Vector3.Normalize(world3DMouse - transform.position);
+                }
+                Vector3 from = transform.position;
+                line.SetPosition(1, from + dir * lineLength);
+                line.SetPosition(0, from);
+
+
+                if(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+                {
+                    GameInput newInput = new GameInput();
+                    newInput.direction = dir;
+                    newInput.use = Input.GetMouseButtonDown(1);
+                    gs.SupplyInput(newInput);
+                }
+                break;
+            case GameSession.State.Processing:
+                break;
+        }
+    }
+
+    void OnEnable()
+    {
+        gs.OnInput += OnInput;
+    }
+    void OnDisable()
+    {
+        gs.OnInput -= OnInput;
+    }
+    void OnInput(GameInput input)
+    {
+        body.velocity = input.direction * speed;
     }
 }
